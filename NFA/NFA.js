a58b2e8a0630aefb7cd2e0677ef10edeaf3aee1e
Bhanu Teja <bhanuteja5481@gmail.com>
2019-01-15 17:08:19 +0530

    Change naming of methods

--- NFA/NFA.js
+++ NFA/NFA.js
@@ -10,46 +10,44 @@ class NFA {
   doesAccept(alphabets) {
     let acceptableStates = this.acceptableStates;
     this.currentStates = [this.startingState];
-    this.currentStates = this.getUpdatedCurrentStates();
+    this.currentStates = this.getEpsilonAppliedStates();
     alphabets.split("").forEach(alphabet => {
-      this.currentStates = this.getCriteriaAppliedStates(alphabet);
+      this.currentStates = this.getNextStates(alphabet);
     });
-    this.currentStates = this.getUpdatedCurrentStates();
+    this.currentStates = this.getEpsilonAppliedStates();
     return this.currentStates.some(state => acceptableStates.includes(state));
   }
 
-  getUpdatedCurrentStates(currentStates = this.currentStates, index = 0) {
+  getEpsilonAppliedStates(currentStates = this.currentStates, index = 0) {
     if(currentStates.length == index) {
       return currentStates;
     }
     currentStates = currentStates.concat(
-      this.getUpdatedLateralCurrentStates(currentStates[index]));
-    return this.getUpdatedCurrentStates(currentStates, ++index);
+      this.getRelativeEpsilonState(currentStates[index]));
+    return this.getEpsilonAppliedStates(currentStates, ++index);
   }
 
-  getUpdatedLateralCurrentStates(state, resultantStates = []) {
+  getRelativeEpsilonState(state, resultantStates = []) {
     if(this.delta[state] == undefined || !this.delta[state]["e"]) {
       return resultantStates;
     }
     let newState = this.delta[state]["e"];
     resultantStates = resultantStates.concat(newState);
-    return this.getUpdatedLateralCurrentStates(newState, resultantStates);
+    return this.getRelativeEpsilonState(newState, resultantStates);
   }
 
-  getCriteriaAppliedStates(alphabet) {
+  getNextStates(alphabet) {
     let newCurrentStates = [];
     this.currentStates.forEach(state => {
       if(alphabet == "") {
         newCurrentStates.push(state);
-      } else if(this.delta[state] == undefined) {
-      } else if(this.delta[state][alphabet]) {
+      } else if(this.delta[state] != undefined && this.delta[state][alphabet]) {
         newCurrentStates = newCurrentStates.concat(this.delta[state][alphabet]);
       }
     });
-    newCurrentStates = this.getUpdatedCurrentStates(newCurrentStates);
+    newCurrentStates = this.getEpsilonAppliedStates(newCurrentStates);
     return newCurrentStates;
   }
-
 }
 
 module.exports = NFA;
